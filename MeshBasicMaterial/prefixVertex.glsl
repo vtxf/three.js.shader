dcc3425b8bd17ab5907e7ee83fedb45b87e9afe0
Tang Xiaofei <txiaofei@aliyun.com>
2016-08-19 17:45:20 +0800

    init commit

---
+++ MeshBasicMaterial/prefixVertex.glsl
@@ -0,0 +1,41 @@
+precision highp float;
+precision highp int;
+#define SHADER_NAME MeshBasicMaterial
+#define VERTEX_TEXTURES
+#define GAMMA_FACTOR 2
+#define MAX_BONES 251
+#define USE_MAP
+#define NUM_CLIPPING_PLANES 0
+uniform mat4 modelMatrix;
+uniform mat4 modelViewMatrix;
+uniform mat4 projectionMatrix;
+uniform mat4 viewMatrix;
+uniform mat3 normalMatrix;
+uniform vec3 cameraPosition;
+attribute vec3 position;
+attribute vec3 normal;
+attribute vec2 uv;
+#ifdef USE_COLOR
+	attribute vec3 color;
+#endif
+#ifdef USE_MORPHTARGETS
+	attribute vec3 morphTarget0;
+	attribute vec3 morphTarget1;
+	attribute vec3 morphTarget2;
+	attribute vec3 morphTarget3;
+	#ifdef USE_MORPHNORMALS
+		attribute vec3 morphNormal0;
+		attribute vec3 morphNormal1;
+		attribute vec3 morphNormal2;
+		attribute vec3 morphNormal3;
+	#else
+		attribute vec3 morphTarget4;
+		attribute vec3 morphTarget5;
+		attribute vec3 morphTarget6;
+		attribute vec3 morphTarget7;
+	#endif
+#endif
+#ifdef USE_SKINNING
+	attribute vec4 skinIndex;
+	attribute vec4 skinWeight;
+#endif
